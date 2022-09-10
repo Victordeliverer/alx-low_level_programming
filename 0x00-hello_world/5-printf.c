c575d194d317e26be3f4331ee5d1ef95d1ca26bf
victordeliverer <deliverervictor@gmail.com>
2022-09-10 13:15:01 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+*this pprogram prints "Programming is like building a multilingual puzzle
+
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
